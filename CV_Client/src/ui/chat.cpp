5bd1759835e7ef70aee3fb6ca31946ae66150fc8
harperchen <742991625@qq.com>
2017-12-04 12:07:41 +0800

    美化

--- CV_Client/src/ui/chat.cpp
+++ CV_Client/src/ui/chat.cpp
@@ -106,14 +106,14 @@ void Chat::recvMsg(QString msg)
 {
     qDebug()<<msg.length();
     if(msg.length()>=23){
-        QString s1="<img width='30' height='30' src='://sys/img/sf.jpg' style='float:left;margin-right:15px;margin-left:15px;'><div style='background:rgb(205,215,226);padding-top:8px;float:left;margin-top:10px;margin-left:45px;padding:10px;margin-right:60px;'><span style='font-size:15px;font-family:'Microsoft YaHei';margin-top:2px;'>"+msg+"</span></div><br><br><br>";
+        QString s1="<img width='40' height='40' src='://sys/img/sf.jpg' style='float:left;margin-right:15px;margin-left:15px;'><div style='background:rgb(205,215,226);padding-top:8px;float:left;margin-top:10px;margin-left:45px;padding:10px;margin-right:60px;'><span style='font-size:15px;font-family:'Microsoft YaHei';margin-top:2px;'>"+msg+"</span></div><br><br><br>";
         ui->textBrowser->insertHtml(s1);
     }
     else{
         int flag=60+(24-msg.length())*15;
         QString myflag=QString::number(flag, 10);;
         qDebug()<<myflag;
-        QString s1="<img width='30' height='30' src='://sys/img/sf.jpg' style='float:left;margin-right:15px;margin-left:15px;'><div style='background:rgb(205,215,226);padding-top:8px;float:left;margin-top:10px;margin-left:45px;padding:10px;margin-right:"+myflag+"px;'>";
+        QString s1="<img width='40' height='40' src='://sys/img/sf.jpg' style='float:left;margin-right:15px;margin-left:15px;'><div style='background:rgb(205,215,226);padding-top:8px;float:left;margin-top:10px;margin-left:45px;padding:10px;margin-right:"+myflag+"px;'>";
         s1+="<span style='font-size:15px;font-family:'Microsoft YaHei';margin-top:2px;'>"+msg+"</span></div><br><br><br>";
         qDebug()<<s1;
         ui->textBrowser->insertHtml(s1);
@@ -167,14 +167,14 @@ void Chat::on_sndBtn_clicked()
     qDebug()<<content.length();
     if(content.length()>=23){
         qDebug()<<"进入了这个";
-        QString s1="<img width='30' height='30' src='qrc:/10.jpeg' style='float:right;margin-left:15px;margin-right:15px;'><div style='background:rgb(205,215,226);padding-top:8px;float:right;margin-top:10px;margin-right:45px;padding:10px;margin-left:60px;'><span style='font-size:15px;font-family:'Microsoft YaHei';margin-top:2px;'>"+content+"</span></div><br><br><br>";
+        QString s1="<img width='40' height='40' src='://sys/img/sf.jpg' style='float:right;margin-left:15px;margin-right:15px;'><div style='background:rgb(205,215,226);padding-top:8px;float:right;margin-top:10px;margin-right:45px;padding:10px;margin-left:60px;'><span style='font-size:15px;font-family:'Microsoft YaHei';margin-top:2px;'>"+content+"</span></div><br><br><br>";
         ui->textBrowser->insertHtml(s1);
     }
     else{
         int flag=60+(24-content.length())*15;
         QString myflag=QString::number(flag, 10);;
         qDebug()<<myflag;
-        QString s1="<img width='30' height='30' src='qrc:/10.jpeg' style='float:right;margin-left:15px;margin-right:15px;'><div style='background:rgb(205,215,226);padding-top:8px;float:right;margin-top:10px;margin-right:45px;padding:10px;margin-left:"+myflag+"px;'>";
+        QString s1="<img width='40' height='40' src='://sys/img/sf.jpg' style='float:right;margin-left:15px;margin-right:15px;'><div style='background:rgb(205,215,226);padding-top:8px;float:right;margin-top:10px;margin-right:45px;padding:10px;margin-left:"+myflag+"px;'>";
         s1+="<span style='font-size:15px;font-family:'Microsoft YaHei';margin-top:2px;'>"+content+"</span></div><br><br><br>";
         qDebug()<<s1;
         ui->textBrowser->insertHtml(s1);
